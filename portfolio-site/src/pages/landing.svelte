2805397646bb7fffeb7cc8a0cda80a2882cd81e5
Sid <sidcarrollworks@gmail.com>
2022-06-16 21:55:10 +0000

    set up svelte

---
+++ portfolio-site/src/pages/landing.svelte
@@ -0,0 +1,13 @@
+
+<div class="w-[100vw] h-[100vh] bg-zinc-100 p-8 text-center flex flex-col justify-center gap-12">
+  <h2 class="text-8xl italic font-serif">
+    Hi I'm
+  </h2>
+  <h1 class="text-9xl font-bold">
+    SID CARROLL
+  </h1>
+  <h2 class="text-4xl font-sans">
+    Graphics + UI Design + Front-end Dev
+  </h2>
+  
+</div>
---
+++ portfolio-site/src/routes/index.svelte
@@ -0,0 +1,9 @@
+<div>
+  <Landing />
+
+</div>
+
+<script>
+	import "../app.css";
+    import Landing from "../pages/landing.svelte"
+</script>
